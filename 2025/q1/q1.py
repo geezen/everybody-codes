9d3ce89b00c3cb97f9f0dc984c44836d05ea8563
marcusgerhardsson <gerhardsson.marcus@gmail.com>
2025-12-01 13:27:24 +0100

    2025 q1 part2

--- 2025/q1/q1.py
+++ 2025/q1/q1.py
@@ -1,5 +1,5 @@
+# part 1
 names, instructions = map(lambda s: s.split(','), open("2025/q1/in1.txt").read().split("\n\n"))
-
 cur = 0
 for instruction in instructions:
     if instruction[0] == 'R':
@@ -9,4 +9,16 @@ for instruction in instructions:
     cur = max(cur, 0)
     cur = min(cur, len(names) - 1)
 
-print(names[cur])
+print(f"part1: {names[cur]}")
+
+# part 2
+names, instructions = map(lambda s: s.split(','), open("2025/q1/in2.txt").read().split("\n\n"))
+cur = 0
+for instruction in instructions:
+    if instruction[0] == 'R':
+        cur += int(instruction[1:])
+    else:
+        cur -= int(instruction[1:])
+cur = cur % len(names)
+
+print(f"part2: {names[cur]}")
