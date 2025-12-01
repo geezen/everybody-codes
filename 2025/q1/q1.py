9f88aad856994e48ffcb83ad63f0df2ddebe65cb
marcusgerhardsson <gerhardsson.marcus@gmail.com>
2025-12-01 13:22:55 +0100

    2025 q1 part1

--- 2025/q1/q1.py
+++ 2025/q1/q1.py
@@ -1,1 +1,12 @@
-print("Hello world!")
+names, instructions = map(lambda s: s.split(','), open("2025/q1/in1.txt").read().split("\n\n"))
+
+cur = 0
+for instruction in instructions:
+    if instruction[0] == 'R':
+        cur += int(instruction[1:])
+    else:
+        cur -= int(instruction[1:])
+    cur = max(cur, 0)
+    cur = min(cur, len(names) - 1)
+
+print(names[cur])
