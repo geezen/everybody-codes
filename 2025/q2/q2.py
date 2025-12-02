fbf8e7e4782309ca6cfb8e3bfe6a39645bb55451
marcusgerhardsson <gerhardsson.marcus@gmail.com>
2025-12-02 12:01:29 +0100

    2025 q2 part2

--- 2025/q2/q2.py
+++ 2025/q2/q2.py
@@ -5,10 +5,27 @@ def div(x, y):
     return complex(copysign(abs(x.real) // y.real, x.real), copysign(abs(x.imag) // y.imag, x.imag))
 
 # part 1
-a = complex(*map(int, re.findall(r"\d+", open("2025/q2/ex1.txt").read())))
+a = complex(*map(int, re.findall(r"-?\d+", open("2025/q2/in1.txt").read())))
 r = 0+0j
 for _ in range(3):
     r = r * r
     r = div(r, 10+10j)
     r = r + a
-print(f"[{r.real},{r.imag}]")
+print(f"[{r.real:.0f},{r.imag:.0f}]")
+
+# part 2
+a = complex(*map(int, re.findall(r"-?\d+", open("2025/q2/in2.txt").read())))
+engraved = 0
+for x in range(101):
+    for y in range(101):
+        p = a + x*10 + y*10j
+        r = 0+0j
+        for _ in range(100):
+            r = r * r
+            r = div(r, 100000+100000j)
+            r = r + p
+            if abs(r.real) > 1000000 or abs(r.imag) > 1000000:
+                break
+        else:
+            engraved += 1
+print(f"engraved points: {engraved}")
