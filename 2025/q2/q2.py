4eb53e236389ff43b5887b514ae4172d90d817eb
marcusgerhardsson <gerhardsson.marcus@gmail.com>
2025-12-02 11:48:22 +0100

    2025 q2 refactored, cleanup and fix part1

--- 2025/q2/q2.py
+++ 2025/q2/q2.py
@@ -1,18 +1,14 @@
-A = (158, 57)
-R = (0, 0)
+import re
+from math import copysign
 
-def add(X, Y):
-    return (X[0] + Y[0], X[1] + Y[1])
+def div(x, y):
+    return complex(copysign(abs(x.real) // y.real, x.real), copysign(abs(x.imag) // y.imag, x.imag))
 
-def multiply(X, Y):
-    return (X[0] * Y[0] - X[1] * Y[1], X[0] * Y[1] + X[1] * Y[0])
-
-def div(X, Y):
-    return (X[0]//Y[0], X[1]//Y[1])
-
+# part 1
+a = complex(*map(int, re.findall(r"\d+", open("2025/q2/ex1.txt").read())))
+r = 0+0j
 for _ in range(3):
-    R = multiply(R, R)
-    R = div(R, (10, 10))
-    R = add(R, A)
-
-print(f"[{R[0]},{R[1]}]")
+    r = r * r
+    r = div(r, 10+10j)
+    r = r + a
+print(f"[{r.real},{r.imag}]")
