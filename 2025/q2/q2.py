32dd7e1062fdd8823309e020bc905a40cfef54e9
marcusgerhardsson <gerhardsson.marcus@gmail.com>
2025-12-03 00:38:34 +0100

    2025 q2 part3

--- 2025/q2/q2.py
+++ 2025/q2/q2.py
@@ -29,3 +29,21 @@ for x in range(101):
         else:
             engraved += 1
 print(f"engraved points: {engraved}")
+
+# part 3
+a = complex(*map(int, re.findall(r"-?\d+", open("2025/q2/in3.txt").read())))
+engraved = 0
+for x in range(1001):
+    for y in range(1001):
+        p = a + x + y*1j
+        r = 0+0j
+        for _ in range(100):
+            r = r * r
+            r = div(r, 100000+100000j)
+            r = r + p
+            if abs(r.real) > 1000000 or abs(r.imag) > 1000000:
+                break
+        else:
+            engraved += 1
+    print(f"{(x/1001*100):.1f}%")
+print(f"engraved points: {engraved}")
