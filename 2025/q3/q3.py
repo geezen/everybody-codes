93711224bd27e86f6eb62d47cd7b38b87c28eece
marcusgerhardsson <gerhardsson.marcus@gmail.com>
2025-12-03 00:54:22 +0100

    2025 q3 part2

--- 2025/q3/q3.py
+++ 2025/q3/q3.py
@@ -1,2 +1,8 @@
-crates = [int(x) for x in open("2025/q3/in1.txt").read().split(',')]
-print(f"part1: {sum(set(crates))}")
+# part1
+crates = {int(x) for x in open("2025/q3/in1.txt").read().split(',')}
+print(f"part1: {sum(crates)}")
+
+# part2
+crates = list({int(x) for x in open("2025/q3/in2.txt").read().split(',')})
+crates.sort()
+print(f"part2: {sum(crates[0:20])}")
