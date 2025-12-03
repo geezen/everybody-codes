3ce00c80edb0aea46b5f62903d93718c76008547
marcusgerhardsson <gerhardsson.marcus@gmail.com>
2025-12-03 13:16:43 +0100

    2025 q4 part3

--- 2025/q4/q4.py
+++ 2025/q4/q4.py
@@ -7,3 +7,12 @@ print(f"part 1: {2025 * gears[0] // gears[-1]}")
 
 # part 2
 print(f"part 2: {math.ceil(10000000000000 * gears[-1]/ gears[0])}")
+
+# part 3
+gears = [tuple(map(int, line.split('|'))) for line in open("2025/q4/in3.txt").readlines()]
+teethturns = 100 * gears[0][0]
+for gear in gears[1:-1]:
+    teethturns /= gear[0]
+    teethturns *= gear[1]
+fullturns = teethturns // gears[-1][0]
+print(f"part3: {fullturns:.0f}")
