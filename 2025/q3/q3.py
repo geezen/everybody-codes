61be3cf18aa0ca54e05c6056d1279e28972b34f1
marcusgerhardsson <gerhardsson.marcus@gmail.com>
2025-12-03 01:02:31 +0100

    2025 q3 part3

--- 2025/q3/q3.py
+++ 2025/q3/q3.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 # part1
 crates = {int(x) for x in open("2025/q3/in1.txt").read().split(',')}
 print(f"part1: {sum(crates)}")
@@ -6,3 +8,7 @@ print(f"part1: {sum(crates)}")
 crates = list({int(x) for x in open("2025/q3/in2.txt").read().split(',')})
 crates.sort()
 print(f"part2: {sum(crates[0:20])}")
+
+#part3
+crates = [int(x) for x in open("2025/q3/in3.txt").read().split(',')]
+print(f"part3: {Counter(crates).most_common(1)[0][1]}")
