57d27c7b3cb71bd576e1896852fc1e9bce0adf47
marcusgerhardsson <gerhardsson.marcus@gmail.com>
2025-12-03 13:02:07 +0100

    2025 q4 part2

--- 2025/q4/q4.py
+++ 2025/q4/q4.py
@@ -1,4 +1,9 @@
-gears = [int(line) for line in open("2025/q4/in1.txt").readlines()]
+import math
+
+gears = [int(line) for line in open("2025/q4/in2.txt").readlines()]
 
 # part 1
 print(f"part 1: {2025 * gears[0] // gears[-1]}")
+
+# part 2
+print(f"part 2: {math.ceil(10000000000000 * gears[-1]/ gears[0])}")
