08edefe5b8d8e5bd596996962637ebda5d20bd46
marcusgerhardsson <gerhardsson.marcus@gmail.com>
2025-12-03 13:36:28 +0100

    2025 q5 part2 temp

--- 2025/q5/q5.py
+++ 2025/q5/q5.py
@@ -1,15 +1,25 @@
-spine = []
-quality = ""
+from math import inf
 
-for num in map(int, open("2025/q5/in1.txt").read().split(':')[1].split(',')):
-    for segment in spine:
-        if num < segment[1] and segment[0] == -1:
-            segment[0] = num
-            break
-        elif num > segment[1] and segment[2] == -1:
-            segment[2] = num
-            break
-    else:
-        spine.append([-1, num, -1])
-        quality += str(num)
-print(quality)
+def getquality(sword):
+    spine = []
+    quality = ""
+    for num in map(int, sword.split(':')[1].split(',')):
+        for segment in spine:
+            if num < segment[1] and segment[0] == -1:
+                segment[0] = num
+                break
+            elif num > segment[1] and segment[2] == -1:
+                segment[2] = num
+                break
+        else:
+            spine.append([-1, num, -1])
+            quality += str(num)
+    return int(quality)
+
+weakest = float(inf)
+strongest = 0
+for sword in open("2025/q5/in2.txt").readlines():
+    quality = getquality(sword)
+    weakest = min(weakest, quality)
+    strongest = max(strongest, quality)
+print(strongest-weakest)
