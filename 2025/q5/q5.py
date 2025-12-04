4cd69c6c8e44c76077b5155853b39f68caee994b
marcusgerhardsson <gerhardsson.marcus@gmail.com>
2025-12-04 21:02:22 +0100

    2025 q5 cleanup

--- 2025/q5/q5.py
+++ 2025/q5/q5.py
@@ -1,5 +1,3 @@
-from math import inf
-
 def parsesword(sword):
     identifier, elements = sword.split(':')
     spine = []
@@ -15,15 +13,15 @@ def parsesword(sword):
             spine.append([-1, num, -1])
     return identifier, spine
 
-def getquality(spine):
+def getquality(sword):
     quality = ""
-    for segment in spine:
+    for segment in sword[1]:
         quality += str(segment[1])
     return int(quality)
 
 def getrank(sword):
     rank = []
-    rank.append(getquality(sword[1]))
+    rank.append(getquality(sword))
     for segment in sword[1]:
         num = ""
         for digit in segment:
@@ -33,11 +31,16 @@ def getrank(sword):
     rank.append(int(sword[0]))
     return rank
 
+# Part 1
+sword = parsesword(open("2025/q5/in1.txt").read())
+quality = getquality(sword)
+print(f"Part 1: {quality}")
+
 # Part 2
-weakest = float(inf)
+weakest = float('inf')
 strongest = 0
-for sword in open("2025/q5/in2.txt").readlines():
-    quality = getquality(parsesword(sword)[1])
+for sword in map(parsesword, open("2025/q5/in2.txt").readlines()):
+    quality = getquality(sword)
     weakest = min(weakest, quality)
     strongest = max(strongest, quality)
 print(f"part 2: {strongest-weakest}")
@@ -46,6 +49,6 @@ print(f"part 2: {strongest-weakest}")
 swords = list(map(parsesword, open("2025/q5/in3.txt").read().splitlines()))
 swords.sort(key=getrank, reverse=True)
 checksum = 0
-for pos, sword in enumerate(swords):
-    checksum += (pos + 1) * int(sword[0])
+for pos, sword in enumerate(swords, 1):
+    checksum += pos * int(sword[0])
 print(f"part 3: {checksum}")
